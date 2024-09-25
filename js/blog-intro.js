799fae795919cfc889d47391d04854aaae464d4f
SenyuYang-math <ysydyx12345@gmail.com>
2024-09-25 21:38:50 +0800

    Create blog-intro.js

---
+++ js/blog-intro.js
@@ -0,0 +1,16 @@
+var bintro = new Array();
+bintro[0] = "null";
+bintro[1] = "一个中学立体几何问题的背后隐藏着Bosuk-Ulam定理.";
+bintro[2]= "详细展示了Dvir的proof from the Book.";
+bintro[3] = "Grownwall不等式的一个非传统证明.";
+bintro[4] = "用微分流形的语言自然地将定理与Stokes公式联系起来."
+bintro[5] = "将分析对象随机化的一个美妙范例.";
+bintro[6] = "令人惊喜的试题，深刻的研究背景.";
+bintro[7] = "Bourgain的两个好工具：定量化和抽屉原理.";
+bintro[8] = "纪念H. Brezis，介绍一道实分析经典习题的背景.";
+bintro[9] = "记录一个内积不等式.";
+bintro[10] = "来自统计物理的计算题，一个漂亮的观察.";
+bintro[11] = "等宽曲线的Barbier定理，以及Crofton公式的一个分析式证明.";
+bintro[12] = "讨论Sobolev嵌入的紧性缺失，叙述了bubble decomposition.";
+bintro[13] = "给出确定尺度的思路和关键工具：频率精细化的Sobolev不等式.";
+// bintro[] = "";
