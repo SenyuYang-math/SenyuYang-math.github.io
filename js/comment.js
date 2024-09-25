0637d928d92aea0736f18e33b677f57cfdc1e96b
SenyuYang-math <ysydyx12345@gmail.com>
2024-09-25 21:40:59 +0800

    Create comment.js

---
+++ js/comment.js
@@ -0,0 +1,13 @@
+// 创建评论区块
+const comment = document.getElementById('container');
+comment.insertAdjacentHTML('afterend', '<div id="vcomments" class="comment"></div>');
+valineConfig = {
+  el: '#vcomments',
+  appId: 'KIRTzIJqIQCgA2ZWFd7x9GFz-gzGzoHsz',
+  appKey: '7lOrkZbew2sPJDrj1b9ROcbX'
+};
+var loadValine = function(){
+              if('Valine' in window) new Valine(valineConfig);
+              else setTimeout(loadValine)
+            };
+loadValine()
