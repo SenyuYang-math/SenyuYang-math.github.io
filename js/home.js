532899de70f73699d5d7dea66d3d8bf4c757d4ac
SenyuYang-math <ysydyx12345@gmail.com>
2024-09-25 21:42:45 +0800

    Create home.js

---
+++ js/home.js
@@ -0,0 +1,31 @@
+// 整体样式
+(function () {
+  var meta = document.createElement('meta');
+  meta.charset = 'utf-8';
+  meta.name = 'viewport';
+  meta.content = 'width=device-width, initial-scale=1.0';
+  document.head.appendChild(meta);
+  var style = document.createElement('link');
+  style.rel = 'stylesheet';
+  style.type = 'text/css';
+  style.href = '../css/style.css';
+  document.head.appendChild(style);
+})();
+// 图标库Font Awesome
+var icon = document.createElement('script');
+icon.src = 'https://kit.fontawesome.com/c61fec31c6.js';
+icon.crossorigin = 'anonymous';
+icon.async = true;
+document.head.appendChild(icon);
+// 配置Mathjax
+var mathjax = document.createElement('script');
+mathjax.src = '../js/math-config.js';
+mathjax.async = true;
+document.body.appendChild(mathjax);
+// 导航栏，页脚
+(function () {
+  var headfoot = document.createElement('script');
+  headfoot.src = '../js/head-foot.js';
+  headfoot.async = true;
+  document.body.appendChild(headfoot);
+})();
