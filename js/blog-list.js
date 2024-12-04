e4b4bf188497d059f0b39032ea9f6553fc0b34c9
Senyu Yang <ysydyx12345@gmail.com>
2024-12-04 21:17:43 +0800

    Update blog-list.js

--- js/blog-list.js
+++ js/blog-list.js
@@ -48,12 +48,12 @@ bottom.insertAdjacentHTML('afterend','<div id="bottom"></div>');
   print.insertAdjacentHTML('beforeend', '<div><p style="text-align:right"><button id="print-button"><i class="fa-solid fa-print"></i> 打印这篇博客</button></p></div>');
   var button = document.getElementById('print-button');
   button.onclick = function() {
-            printJS({printable: 'container', type: 'html', scanStyles: false, css: 'https://senyuyang-math.github.io/css/style.css', style: `@page { size: A4 portrait; margin: 1.3cm 1.4cm}`);
-            // var originContents = document.body.innerHTML;
-            // var actContents = document.getElementById("container").innerHTML;
-            // document.body.innerHTML = actContents;
-            // window.print();
-            // document.body.innerHTML= originContents;
+            // printJS({printable: 'container', type: 'html', scanStyles: false, css: 'https://senyuyang-math.github.io/css/style.css', style: `@page { size: A4 portrait; margin: 1.3cm 1.4cm}`);
+            var originContents = document.body.innerHTML;
+            var actContents = document.getElementById("container").innerHTML;
+            document.body.innerHTML = actContents;
+            window.print();
+            document.body.innerHTML= originContents;
   }
 
   // 创建版权许可
