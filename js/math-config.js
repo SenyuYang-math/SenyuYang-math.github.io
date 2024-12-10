b7c2e899885077ee904eadc68c9d6075cf1016de
Senyu Yang <ysydyx12345@gmail.com>
2024-12-10 17:11:14 +0800

    Update math-config.js

--- js/math-config.js
+++ js/math-config.js
@@ -34,3 +34,11 @@
     // }
   }
 };
+(function () {
+  var jaxcdn = document.createElement('script');
+  // jaxcdn.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.0.0/es5/tex-mml-chtml.js';
+  jaxcdn.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.2/es5/tex-chtml.min.js';
+  // jaxcdn.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
+  jaxcdn.async = true;
+  document.body.appendChild(jaxcdn);
+})();
