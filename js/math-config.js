876bafbf09cb85ab76fad0e3343aa9cd858f7524
Senyu Yang <ysydyx12345@gmail.com>
2024-12-10 17:22:39 +0800

    Update math-config.js

--- js/math-config.js
+++ js/math-config.js
@@ -32,12 +32,16 @@
     //   CN: "{\\mathcal{N}}", 
     //   p: "{\\partial}",
     // }
+  }, 
+  svg: {
+    fontCache: 'global'
   }
 };
 (function () {
   var jaxcdn = document.createElement('script');
   // jaxcdn.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.0.0/es5/tex-mml-chtml.js';
-  jaxcdn.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.2/es5/tex-chtml.min.js';
+  // jaxcdn.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.2/es5/tex-chtml.min.js';
+     jaxcdn.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.2/es5/tex-svg.min.js';
   // jaxcdn.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
   jaxcdn.async = true;
   document.body.appendChild(jaxcdn);
