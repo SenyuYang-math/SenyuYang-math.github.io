0457f6fb7564964b14688bf47b08d647e6baf987
Senyu Yang <ysydyx12345@gmail.com>
2024-12-10 15:39:59 +0800

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,4 @@
 // 整体样式
-(function () {
   var meta = document.createElement('meta');
   meta.charset = 'utf-8';
   meta.name = 'viewport';
@@ -10,7 +9,6 @@
   style.type = 'text/css';
   style.href = '../css/style.css';
   document.head.appendChild(style);
-})();
 // 图标库Font Awesome
 var icon = document.createElement('script');
 icon.src = 'https://kit.fontawesome.com/c61fec31c6.js';
@@ -18,33 +16,27 @@ icon.crossorigin = 'anonymous';
 icon.async = true;
 document.head.appendChild(icon);
 // 文章标题, 正文末尾
-(function () {
   var blogtitle = document.createElement('script');
   blogtitle.src = '../js/blog-list.js';
   blogtitle.async = true;
   document.body.appendChild(blogtitle);
-})();
 // 配置Mathjax
 var mathjax = document.createElement('script');
 mathjax.src = '../js/math-config.js';
 mathjax.type = 'text/x-mathjax-config';
 mathjax.async = true;
 document.body.appendChild(mathjax);
-(function () {
   var jaxcdn = document.createElement('script');
-  // script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.0.0/es5/tex-mml-chtml.js';
-  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TEX-MML-AM_CHTML';
-  // script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
-  script.async = true;
+  // jaxcdn.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.0.0/es5/tex-mml-chtml.js';
+  jaxcdn.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TEX-MML-AM_CHTML';
+  // jaxcdn.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
+  jaxcdn.async = true;
   document.body.appendChild(jaxcdn);
-})();
 // 导航栏，页脚
-(function () {
   var headfoot = document.createElement('script');
   headfoot.src = '../js/head-foot.js';
   headfoot.async = true;
   document.body.appendChild(headfoot);
-})();
 // 打印组件
 var pr = document.createElement('script');
 pr.src = 'https://printjs-4de6.kxcdn.com/print.min.js';
