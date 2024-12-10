df71e9614182e34b6e3d31775c07f8725f8ae5ad
Senyu Yang <ysydyx12345@gmail.com>
2024-12-10 17:00:04 +0800

    Update math-config.js

--- js/math-config.js
+++ js/math-config.js
@@ -1,11 +1,11 @@
 // 2.7.5版本配置
-MathJax.Hub.Config({
-  showProcessingMessages: true,
-  messageStyle: "normal",
-  jax: ["input/TeX", "output/HTML-CSS"], 
-  tex2jax: {
-    inlineMath: [['$', '$']], 
-    displayMath: [['$$', '$$']],
+// MathJax.Hub.Config({
+//   showProcessingMessages: true,
+//   messageStyle: "normal",
+//   jax: ["input/TeX", "output/HTML-CSS"], 
+//   tex2jax: {
+//     inlineMath: [['$', '$']], 
+//     displayMath: [['$$', '$$']],
     //     macros: {
     //       RR: "{\\bf R}",
     //       CH: "{\\mathcal{H}}", 
@@ -13,24 +13,24 @@ MathJax.Hub.Config({
     //       p: "{\\partial}",
     //     }
   }
-  "HTML-CSS": {
-    availableFonts: ["TeX"], 
-    showMathMenu: false,
-  }
-});
-// 3.0.0版本配置
-  // window.MathJax = {
-  // options: {
-  //   enableMenu: false
-  // }, 
-//   tex: {
-//     inlineMath: [['$', '$']], 
-//     displayMath: [['$$', '$$']],
-//     macros: {
-//       RR: "{\\bf R}",
-//       CH: "{\\mathcal{H}}", 
-//       CN: "{\\mathcal{N}}", 
-//       p: "{\\partial}",
-//     }
+//   "HTML-CSS": {
+//     availableFonts: ["TeX"], 
+//     showMathMenu: false,
 //   }
-// };
+// });
+// 3.0.0版本配置
+  window.MathJax = {
+  options: {
+    enableMenu: false
+  }, 
+  tex: {
+    inlineMath: [['$', '$']], 
+    displayMath: [['$$', '$$']],
+    // macros: {
+    //   RR: "{\\bf R}",
+    //   CH: "{\\mathcal{H}}", 
+    //   CN: "{\\mathcal{N}}", 
+    //   p: "{\\partial}",
+    // }
+  }
+};
