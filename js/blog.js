51470a711552f45fc31dc1ced3c84fd3ca8a0187
Senyu Yang <ysydyx12345@gmail.com>
2024-12-10 16:34:46 +0800

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -9,6 +9,11 @@
   style.type = 'text/css';
   style.href = '../css/style.css';
   document.head.appendChild(style);
+// 导航栏，页脚
+  var headfoot = document.createElement('script');
+  headfoot.src = '../js/head-foot.js';
+  headfoot.async = true;
+  document.body.appendChild(headfoot);
 // 图标库Font Awesome
 var icon = document.createElement('script');
 icon.src = 'https://kit.fontawesome.com/c61fec31c6.js';
@@ -24,19 +29,14 @@ document.head.appendChild(icon);
 var mathjax = document.createElement('script');
 mathjax.src = '../js/math-config.js';
 mathjax.type = 'text/x-mathjax-config';
-mathjax.async = false;
+mathjax.async = true;
 document.body.appendChild(mathjax);
   var jaxcdn = document.createElement('script');
   // jaxcdn.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.0.0/es5/tex-mml-chtml.js';
-  jaxcdn.src = '//cdn.bootcss.com/mathjax/2.7.0/MathJax.js?config=TeX-AMS-MML_HTMLorMML';
+  jaxcdn.src = '//cdn.bootcss.com/mathjax/2.7.5/MathJax.js?config=TeX-AMS-MML_HTMLorMML';
   // jaxcdn.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
-  jaxcdn.async = true;
+  jaxcdn.async = defer;
   document.body.appendChild(jaxcdn);
-// 导航栏，页脚
-  var headfoot = document.createElement('script');
-  headfoot.src = '../js/head-foot.js';
-  headfoot.async = true;
-  document.body.appendChild(headfoot);
 // 打印组件
 var pr = document.createElement('script');
 pr.src = 'https://printjs-4de6.kxcdn.com/print.min.js';
