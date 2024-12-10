8f736ca41919bba09b67360e51e5805b57c453c7
Senyu Yang <ysydyx12345@gmail.com>
2024-12-10 15:07:35 +0800

    Update math-config.js

--- js/math-config.js
+++ js/math-config.js
@@ -1,23 +1,44 @@
-window.MathJax = {
-  options: {
-    enableMenu: false
-  },
-  tex: {
-    inlineMath: [['$', '$']], 
-    displayMath: [['$$', '$$']],
-    macros: {
-      RR: "{\\bf R}",
-      CH: "{\\mathcal{H}}", 
-      CN: "{\\mathcal{N}}", 
-      p: "{\\partial}"
-    }
+window.MathJax.Hub.Config({
+  showProcessingMessages: true,
+  messageStyle: "none",
+  jax: ["input/TeX", "output/HTML-CSS"], 
+  tex2jax: {
+    inlineMath: [["$", "$"]], 
+    displayMath: [["$$", "$$"]],
+    skipTags: ["script", "script", "head"], 
+    //     macros: {
+    //       RR: "{\\bf R}",
+    //       CH: "{\\mathcal{H}}", 
+    //       CN: "{\\mathcal{N}}", 
+    //       p: "{\\partial}",
+    //     }
   }
-};
+  "HTML-CSS": {
+    availableFonts: ["STIX", "TeX"], 
+    showMathMenu: false,
+  }
+});
+// 3.0.0版本配置
+  // window.MathJax = {
+  // options: {
+  //   enableMenu: false
+  // }, 
+//   tex: {
+//     inlineMath: [['$', '$']], 
+//     displayMath: [['$$', '$$']],
+//     macros: {
+//       RR: "{\\bf R}",
+//       CH: "{\\mathcal{H}}", 
+//       CN: "{\\mathcal{N}}", 
+//       p: "{\\partial}",
+//     }
+//   }
+// };
 
 (function () {
   var script = document.createElement('script');
-  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.0.0/es5/tex-mml-chtml.js';
-  // script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TEX-MML-AM_CHTML';
+  // script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.0.0/es5/tex-mml-chtml.js';
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TEX-MML-AM_CHTML';
   // script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
   script.async = true;
   document.head.appendChild(script);
