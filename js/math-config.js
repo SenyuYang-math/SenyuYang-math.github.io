e46845ecdc07a55dca5255774e9c84e040783543
Senyu Yang <ysydyx12345@gmail.com>
2024-12-10 16:08:00 +0800

    Update math-config.js

--- js/math-config.js
+++ js/math-config.js
@@ -34,12 +34,3 @@ MathJax.Hub.Config({
 //     }
 //   }
 // };
-
-(function () {
-  var script = document.createElement('script');
-  // script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.0.0/es5/tex-mml-chtml.js';
-  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TEX-MML-AM_CHTML';
-  // script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
-  script.async = true;
-  document.head.appendChild(script);
-})();
