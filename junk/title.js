b9d539409ccfa0d852f8123a290e0f6d59429884
Senyu Yang <ysydyx12345@gmail.com>
2024-12-23 21:21:03 +0800

    Delete junk directory

--- junk/title.js
+++
@@ -1,8 +0,0 @@
-//文章标题区块
-  const titleblock = document.getElementById('container');
-  titleblock.insertAdjacentHTML('afterbegin', '<div id="titleblock"></div>');
-  fetch('../../temp/blog/title.html')
-    .then(response => response.text())
-    .then(data => {
-    document.querySelector('#titleblock').innerHTML = data;
-    });
