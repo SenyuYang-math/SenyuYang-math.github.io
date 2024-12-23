c2e0fd0999ad750b7328c2bdf9b1b147d5bcd000
Senyu Yang <ysydyx12345@gmail.com>
2024-12-23 20:08:51 +0800

    Update post.js

--- js/blog/post.js
+++ js/blog/post.js
@@ -20,6 +20,21 @@ var number = Number(urltxt.substring(11,urllength-5));
   headfoot.src = '../../js/head-foot.js';
   headfoot.async = true;
   document.body.appendChild(headfoot);
+//文章标题区块
+  const titleblock = document.getElementById('container');
+  titleblock.insertAdjacentHTML('afterbegin', '<div id="titleblock"></div>');
+  fetch('../../temp//blog/title.html')
+    .then(response => response.text())
+    .then(data => {
+    document.querySelector('#titleblock').innerHTML = data;
+    });
+// 导入文章信息
+  window.onload=function(){
+    var blogtitle = document.createElement('script');
+    blogtitle.src = '../../js/blog/list/' + year + '.js';
+    blogtitle.async = true;
+    document.body.appendChild(blogtitle);
+  };
 // 配置Mathjax
 var mathjax = document.createElement('script');
 mathjax.src = '../../js/math-config.js';
