a65c5e242c17a5814da297189a4acb43c091d809
Senyu Yang <ysydyx12345@gmail.com>
2024-12-23 20:27:42 +0800

    Update title.js

--- js/blog/title.js
+++ js/blog/title.js
@@ -1,7 +1,7 @@
 //文章标题区块
   const titleblock = document.getElementById('container');
   titleblock.insertAdjacentHTML('afterbegin', '<div id="titleblock"></div>');
-  fetch('../../temp//blog/title.html')
+  fetch('../../temp/blog/title.html')
     .then(response => response.text())
     .then(data => {
     document.querySelector('#titleblock').innerHTML = data;
