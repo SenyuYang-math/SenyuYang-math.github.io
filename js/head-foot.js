851d00a7ecf0dd3dc039f8f0e63e717c68648092
Senyu Yang <ysydyx12345@gmail.com>
2024-12-23 14:07:58 +0800

    Update head-foot.js

--- js/head-foot.js
+++ js/head-foot.js
@@ -4,12 +4,12 @@ headnav.insertAdjacentHTML('afterbegin', '<div id="headnav" class="head"></div>'
 const footbar = document.getElementById('body');
 footbar.insertAdjacentHTML('beforeend', '<div id="footbar" class="foot"></div>');
 // 调用html模板
-fetch('senyuyang-math.github.io/temp/head.html')
+fetch('https://senyuyang-math.github.io/temp/head.html')
             .then(response => response.text())
             .then(data => {
                 document.querySelector('#headnav').innerHTML = data;
             });
-fetch('senyuyang-math.github.io/temp/foot.html')
+fetch('https://senyuyang-math.github.io/temp/foot.html')
             .then(response => response.text())
             .then(data => {
                 document.querySelector('#footbar').innerHTML = data;
