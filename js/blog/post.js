025afddae7cbcc0c35af6f49b5aac401ad3c7bff
Senyu Yang <ysydyx12345@gmail.com>
2024-12-23 20:58:37 +0800

    Update post.js

--- js/blog/post.js
+++ js/blog/post.js
@@ -32,6 +32,7 @@ function infoinput() {
     bloginfo.async = true;
     document.body.appendChild(bloginfo);
 }
+// 文章标题区块
 async function titleload() {
 const titleblock = document.getElementById('container');
   titleblock.insertAdjacentHTML('afterbegin', '<div id="titleblock"></div>');
@@ -47,19 +48,15 @@ bottom.insertAdjacentHTML('afterend','<div id="bottom"></div>');
   // 创建版权许可
   const blogcopy = document.getElementById('bottom');
   blogcopy.insertAdjacentHTML('beforeend', '<div id="copyright"></div>');
-  fetch('../../temp/blog/copyright.html')
-    .then(response => response.text())
-    .then(data => {
+  fetch('../../temp/blog/copyright.html').then(response => response.text()).then(data => {
     document.querySelector('#copyright').innerHTML = data;
   });
 // valine评论系统
 var valine = document.createElement('script');
-valine.src = 'https://unpkg.com/valine/dist/Valine.min.js';
-valine.async = true;
-document.head.appendChild(valine);
-window.onload = (function () {
-  var comment = document.createElement('script');
+  valine.src = 'https://unpkg.com/valine/dist/Valine.min.js';
+  valine.async = true;
+  document.head.appendChild(valine);
+var comment = document.createElement('script');
   comment.src = '../../js/blog/comment.js';
   comment.async = true;
   document.body.appendChild(comment);
-})();
