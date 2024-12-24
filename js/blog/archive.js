9e4b1fa97fa2a412d690519889bd2a2415bd9935
Senyu Yang <ysydyx12345@gmail.com>
2024-12-24 15:38:50 +0800

    Update archive.js

--- js/blog/archive.js
+++ js/blog/archive.js
@@ -29,16 +29,8 @@ document.body.appendChild(mathjax);
   document.body.appendChild(headfoot);
 //导入博客列表
   var archYear = Number(urltxt.substring(6, 10));;
-  // var archive = document.createElement('script');
-  // archive.id = 'blog-list';
-  // archive.src = '../../js/blog/list/' + archYear + '.js';
-  // archive.async = true;
-  // document.body.appendChild(archive);
-  var list = '../../js/blog/modules/' + archYear + '.js'
-  let { btitle, bdate, bintro, amount } = await import(list);
-//生成目录
-document.getElementById('blogindex').insertAdjacentHTML('beforebegin', '<div id="year"><span style="display:table-cell; vertical-align:bottom; float:right"><a href="../home.html"><b>返回主页</b></a></span><h2>' + archYear + '</h2></div>');
-var i = 1;
-for (i=1; i < amount; i++) {
-    document.getElementById('blogindex').insertAdjacentHTML('afterbegin', '<div class="card"><div class="cardcontent"><a href="./' + i + '.html"><div class="cardlink"><p class="time">' + bdate[i] + '</p><p class="title">' + btitle[i] + '</p><p class="intro">' + bintro[i] + '</p></div></a></div></div>');
-  } 
+  var archive = document.createElement('script');
+  archive.id = 'blog-list';
+  archive.src = '../../js/blog/list/' + archYear + '.js';
+  archive.async = true;
+  document.body.appendChild(archive);
