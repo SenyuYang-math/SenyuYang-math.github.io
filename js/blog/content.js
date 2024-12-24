cf45d0eead8a9f6766ab4174bba2a33183aa19c9
Senyu Yang <ysydyx12345@gmail.com>
2024-12-24 16:04:04 +0800

    Update content.js

--- js/blog/content.js
+++ js/blog/content.js
@@ -1,4 +1,4 @@
-document.getElementById('blogindex').insertAdjacentHTML('beforebegin', '<span style="float:right; display: table-cell; vertical-align:bottom"><a href="../home.html"><b>返回主页</b></a></span><div id="year"><h2>' + archYear + '</h2></div>');
+document.getElementById('blogindex').insertAdjacentHTML('beforebegin', '<span style="float:right"><a href="../home.html"><b>返回主页</b></a></span><h2>' + archYear + '</h2>');
 for (i=1; i < amount; i++) {
     document.getElementById('blogindex').insertAdjacentHTML('afterbegin', '<div class="card"><div class="cardcontent"><a href="./' + i + '.html"><div class="cardlink"><p class="time">' + bdate[i] + '</p><p class="title">' + btitle[i] + '</p><p class="intro">' + bintro[i] + '</p></div></a></div></div>');
   } 
