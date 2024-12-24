49dff9c2e4f195523c14baeb2a107f301996f927
Senyu Yang <ysydyx12345@gmail.com>
2024-12-24 17:02:51 +0800

    Update 2025.js

--- js/blog/list/2025.js
+++ js/blog/list/2025.js
@@ -4,12 +4,12 @@ btitle[0] = "null"; bdate[0] = "null"; bintro[0] = "null";
 // btitle[] = ""; bdate[] = ""; bintro[] = "";
 var amount = btitle.length;
 
-// 博客文章页面
+// 博客文章标题
 if (urllength < 20) {
-document.getElementById('blogtitle').innerHTML = btitle[number];
-document.getElementById('blogdate').innerHTML = '2025/' + bdate[number];
+  document.getElementById('blogtitle').innerHTML = btitle[number];
+  document.getElementById('blogdate').innerHTML = '<a href="./archive.html" style="text-decoration:none">2025</a>/' + bdate[number];
 } else {
-// 博客主页目录
+// 该年份目录
   var homepage = document.createElement('script');
   homepage.src = '../../js/blog/content.js';
   homepage.async = true;
