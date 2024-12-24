6284ea23c15166801e961167e38d70c2c643533e
Senyu Yang <ysydyx12345@gmail.com>
2024-12-24 16:59:18 +0800

    Update 2024.js

--- js/blog/list/2024.js
+++ js/blog/list/2024.js
@@ -29,7 +29,7 @@ var amount = btitle.length;
 // 博客文章标题
 if (urllength < 20) {
   document.getElementById('blogtitle').innerHTML = btitle[number];
-  document.getElementById('blogdate').innerHTML = '<a href="./archive.html">2024</a>/' + bdate[number];
+  document.getElementById('blogdate').innerHTML = '<a href="./archive.html" style="text-decoration:none">2024</a>/' + bdate[number];
 } else {
 // 该年份目录
   var homepage = document.createElement('script');
