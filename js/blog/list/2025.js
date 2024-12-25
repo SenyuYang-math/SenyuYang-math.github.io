720b2a8a201d5a2698983b2745dd98c373c808fc
Senyu Yang <ysydyx12345@gmail.com>
2024-12-25 18:41:08 +0800

    Update 2025.js

--- js/blog/list/2025.js
+++ js/blog/list/2025.js
@@ -1,7 +1,7 @@
 // 文章列表
 var btitle = new Array(); var bdate = new Array(); var bintro = new Array();
 btitle[0] = "null"; bdate[0] = "null"; bintro[0] = "null";
-btitle[1] = "不确定性原理，Cauchy-Davenport定理与分圆数"; bdate[1] = "01/01"; bintro[1] = "Z/pZ上的调和分析同代数数论和加性组合的奇妙联系";
+btitle[1] = "不确定性原理，Cauchy-Davenport定理与分圆数"; bdate[1] = "01/01"; bintro[1] = "Z/pZ的调和分析同代数数论和加性组合的奇妙联系";
 // btitle[] = ""; bdate[] = ""; bintro[] = "";
 var amount = btitle.length;
 
