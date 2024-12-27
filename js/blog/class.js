1838937a05c4de7a563191df1dd511ff20fbf633
Senyu Yang <ysydyx12345@gmail.com>
2024-12-27 13:12:13 +0800

    Update class.js

--- js/blog/class.js
+++ js/blog/class.js
@@ -1,7 +1,7 @@
 // 博客文章标题
 if (urllength < 20) {
   document.getElementById('blogtitle').innerHTML = btitle[number];
-  document.getElementById('blogdate').innerHTML = '<a href="./archive.html" style="text-decoration:none">' + year + '</a>/' + bdate[number];
+  document.getElementById('blogdate').innerHTML = year + '/' + bdate[number];
 } else {
 // 该年份目录
   var caseArchive = document.createElement('script');
