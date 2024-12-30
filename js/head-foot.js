ec64e5f1de2f850c1dabd1411c82eacee8f61dd5
Senyu Yang <ysydyx12345@gmail.com>
2024-12-30 21:46:29 +0800

    Update head-foot.js

--- js/head-foot.js
+++ js/head-foot.js
@@ -14,5 +14,5 @@ await fetch('https://senyuyang-math.github.io/temp/foot.html').then(response => 
                 document.querySelector('#footbar').innerHTML = data;
             });
 document.getElementById('sign').insertAdjacentHTML('afterbegin', '&copy; 2024-' + currYear + ' Senyu Yang');
-});
+};
 footHTML();
