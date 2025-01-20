bbdbbc66f8741bcdf9bce57fa2e153a4dcb02279
Senyu Yang <ysydyx12345@gmail.com>
2025-01-20 17:41:08 +0800

    Update 2025.js

--- js/blog/list/2025.js
+++ js/blog/list/2025.js
@@ -3,7 +3,7 @@ var btitle = new Array(); var bdate = new Array(); var bintro = new Array();
 btitle[0] = "null"; bdate[0] = "null"; bintro[0] = "null";
 btitle[1] = "不确定性原理，Cauchy-Davenport定理与分圆数"; bdate[1] = "01/01"; bintro[1] = "Z/pZ的调和分析，以及与代数数论和加性组合的奇妙联系.";
 btitle[2] = "递推数列的微扰与稳定性"; bdate[2] = "01/14"; bintro[2] = "来自H. Poincaré的结果，关键在于刻画递推向量各个分量的渐进行为";
-btitle[3] = "Erdӧs方法与一个Fourier变换的反例"; bdate[3] = "01/20"; bintro[3] = "随机化思想在调和分析的又一体现.";
+btitle[3] = "Erdӧs方法与Hausdorff-Young不等式的反例"; bdate[3] = "01/20"; bintro[3] = "随机化思想在调和分析的又一体现.";
 // btitle[] = ""; bdate[] = ""; bintro[] = "";
 var amount = btitle.length;
 
