507e6f5f9d7b3a20f479fc0f6883de3c28f57693
Senyu Yang <ysydyx12345@gmail.com>
2025-05-31 20:22:50 +0800

    Update class.js

--- js/blog/class.js
+++ js/blog/class.js
@@ -3,6 +3,11 @@ if (urllength > 15) {
   document.getElementById('blogtitle').innerHTML = btitle[number];
   document.getElementById('blogdate').innerHTML = year + '/' + bdate[number];
   document.getElementById('pagetitle').innerHTML = btitle[number];
+  // 配置Mathjax
+var mathjax = document.createElement('script');
+mathjax.src = '../../js/math-config.js';
+mathjax.async = true;
+document.body.appendChild(mathjax);
 } else {
 // 该年份目录
   var caseArchive = document.createElement('script');
