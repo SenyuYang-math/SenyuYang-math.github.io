0036363cdd2fff83ff204ac6e9a46a86a71df461
Senyu Yang <ysydyx12345@gmail.com>
2025-05-31 20:23:42 +0800

    Update content.js

--- js/blog/content.js
+++ js/blog/content.js
@@ -1,4 +1,9 @@
 document.getElementById('blogindex').insertAdjacentHTML('beforebegin', '<p style="margin:20px 0; padding:0"><span style="font-size:25px"><b>' + archYear + '</b></span></p>');
 for (i=1; i < amount; i++) {
     document.getElementById('blogindex').insertAdjacentHTML('afterbegin', '<div class="card"><div class="cardcontent"><a href="./' + archYear + '/' + i + '.html"><div class="cardlink"><p class="time">' + bdate[i] + '</p><p class="title">' + btitle[i] + '</p><p class="intro">' + bintro[i] + '</p></div></a></div></div>');
-  } 
+  };
+// 配置Mathjax
+var mathjax = document.createElement('script');
+mathjax.src = '../../js/math-config.js';
+mathjax.async = true;
+document.body.appendChild(mathjax)
