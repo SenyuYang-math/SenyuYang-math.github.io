58653366154ea1912cff059f28112074a9ce1f93
Senyu Yang <ysydyx12345@gmail.com>
2025-12-05 17:03:03 +0800

    Update math-config.js

--- js/math-config.js
+++ js/math-config.js
@@ -54,7 +54,7 @@
   // jaxcdn.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.0.0/es5/tex-mml-chtml.js';
   // jaxcdn.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.2/es5/tex-chtml.min.js';
   // jaxcdn.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.2/es5/tex-svg.min.js';
-  // jaxcdn.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+  jaxcdn.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
   jaxcdn.async = true;
   document.body.appendChild(jaxcdn);
 })();
