27cdd1a205428c40e7de9b2a16af57630176bdb3
Senyu Yang <ysydyx12345@gmail.com>
2026-01-26 12:06:23 +0800

    Create 2026.js

---
+++ js/blog/list/2026.js
@@ -0,0 +1,11 @@
+// 文章列表
+var btitle = new Array(); var bdate = new Array(); var bintro = new Array();
+btitle[0] = "null"; bdate[0] = "null"; bintro[0] = "null";
+btitle[1] = "分析学家的Szemerédi正则引理"; bdate[1] = "01/26"; bintro[1] = "用对称函数的 $L^2$ 理论和cut norm刻画图的弱正则性.";
+// btitle[] = ""; bdate[] = ""; bintro[] = "";
+var amount = btitle.length;
+
+  var classify = document.createElement('script');
+  classify.src = '../../js/blog/class.js';
+  classify.async = true;
+  document.body.appendChild(classify);
