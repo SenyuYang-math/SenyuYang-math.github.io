e8e20467e31a1d9eac43fee124729e86c64edd9e
Senyu Yang <ysydyx12345@gmail.com>
2026-09-17 01:55:08 +0800

    Update 2026.js

--- js/blog/list/2026.js
+++ js/blog/list/2026.js
@@ -4,7 +4,7 @@ btitle[0] = "null"; bdate[0] = "null"; bintro[0] = "null";
 btitle[1] = "分析学家的Szemerédi正则引理"; bdate[1] = "01/26"; bintro[1] = "用对称函数的L^2理论和cut norm刻画图的弱正则性.";
 btitle[2] = "环面上的Kiselev-Sverak引理"; bdate[2] = "03/30"; bintro[2] = "二维Euler方程速度场的分解，对应了一种双曲流的拉伸效应.";
 btitle[3] = "调和函数的频率"; bdate[3] = "05/02"; bintro[3] = "调和函数在球面上的振荡频率控制了径向的增长.";
-btitle[4] = "非平凡铺满欧氏空间的直线族"; bdate[4] = "09/17"; bintro[4] = "考古一道UyHiP上的趣题."
+btitle[4] = "非平凡铺满欧氏空间的直线族"; bdate[4] = "09/17"; bintro[4] = "考古UyHiP上的一道趣题."
 // btitle[] = ""; bdate[] = ""; bintro[] = "";
 var amount = btitle.length;
 
