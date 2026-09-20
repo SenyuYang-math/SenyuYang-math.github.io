e621728afb2e81624320b5983627a08b33dd23f7
Senyu Yang <ysydyx12345@gmail.com>
2026-09-20 17:18:27 +0800

    Update 2026.js

--- js/blog/list/2026.js
+++ js/blog/list/2026.js
@@ -5,7 +5,7 @@ btitle[1] = "分析学家的Szemerédi正则引理"; bdate[1] = "01/26"; bintro[
 btitle[2] = "环面上的Kiselev-Sverak引理"; bdate[2] = "03/30"; bintro[2] = "二维Euler方程速度场的分解，对应了一种双曲流的拉伸效应.";
 btitle[3] = "调和函数的频率"; bdate[3] = "05/02"; bintro[3] = "调和函数在球面上的振荡频率控制了径向的增长.";
 btitle[4] = "非平凡铺满欧氏空间的直线族"; bdate[4] = "09/17"; bintro[4] = "考古UyHiP上的一道趣题.";
-btitle[5] = "一个Pólya定理 (与Hardy的改进)"; bdate[5] = "09/20"; bintro[5] = "整函数在算术集上的取值给出了增长行为的分类.";
+btitle[5] = "一个Pólya定理(与Hardy的改进)"; bdate[5] = "09/20"; bintro[5] = "整函数在算术集上的取值给出了增长行为的分类.";
 // btitle[] = ""; bdate[] = ""; bintro[] = "";
 var amount = btitle.length;
 
